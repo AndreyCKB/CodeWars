a4b44cb71e28bcc3ce0751982182d04ae73e4342
Andrey Lyahovich <AndreyCKB@mail.ru>
2020-06-27 13:51:09 +0500

    Tasks:       JadenCasingStrings       PlayingWithDigits solved

--- src/main/java/kyu8/DescendingOrder.java
+++ src/main/java/kyu8/DescendingOrder.java
@@ -14,51 +14,28 @@ import java.util.Arrays;
 public class DescendingOrder {
     public static int sortDesc(final int num) {
         //Your code
-        if (num > -10 && num < 10) return num;
+        if (num > -10 && num < 10)
+            return num;
         byte[] arr = intSplitToDigit(num);
-        sortArrayByte(arr);
+        Arrays.sort(arr);
         return arrayByteToInt(arr);
     }
+
     public static byte[] intSplitToDigit(int number){
         byte count = 0;
-        byte[] result =new byte[11];
-        while (number >= 1){
+        byte[] result = new byte[10];
+        while (number > 0){
             result[ count ] = (byte) (number % 10);
             number /= 10;
             ++count;
         }
-        result[ 10 ] = count;
-        return result;
-    }
-
-    public static void sortArrayByte(byte[] arr){
-        byte endIndex = arr[ arr.length - 1 ];
-        if ( endIndex  <= 1) return;
-        byte curentIndex = 1;
-        byte tempIndex = curentIndex;
-        byte temp = 0;
-        while (curentIndex < endIndex){
-            if (arr[ tempIndex ] > arr[ tempIndex -1 ]){
-                temp = arr[ tempIndex ];
-                arr[ tempIndex ] = arr [ tempIndex -1 ];
-                arr[ tempIndex - 1 ] = temp;
-                if (tempIndex != 1) tempIndex--;
-            } else {
-                tempIndex = ++curentIndex;
-            }
-        }
+        return Arrays.copyOf(result, count);
     }
 
     public static int arrayByteToInt(byte[] arr){
-        byte length = arr[ arr.length - 1 ];
-        if ( length  <= 1) return arr[ 0 ];
-        int multiplier = 1;
-        int result = 0;
-        for (int i = --length; i > -1; i--){
-            if(arr[ i ] != 0){
-                result += arr[i] * multiplier;
-            }
-            multiplier *= 10;
+        int result = arr[ arr.length -1 ];
+        for (int i = arr.length - 2; i > - 1; i-- ){
+            result = result*10 + arr[i];
         }
         return result;
     }
